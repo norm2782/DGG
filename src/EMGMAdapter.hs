cdadedf7be77721dd65e82b23192e679ceaff8de
Jurriën Stutterheim <j.stutterheim@me.com>
2010-12-24 15:08:04 +0100

    Generate EP based on value constructor arity

--- src/EMGMAdapter.hs
+++ src/EMGMAdapter.hs
@@ -35,23 +35,23 @@ bdecls vcis = BDecls $ (P.map (bdeclFrom ln) vcis) ++ (P.map (bdeclTo ln) vcis)
     where ln = length vcis
 
 bdeclFrom :: Int -> VCInfo -> Decl
-bdeclFrom cnt vci@(VCInfo n _ _ _ _ rs) =
+bdeclFrom cnt vci@(VCInfo n a _ _ _ _) =
     FunBind [Match srcLoc (Ident fromFunName) 
-        [PApp (unQualIdent n) (P.map (pVarIdent . recTyname) rs)]
+        [PApp (unQualIdent n) (P.map pVarIdent (genNames a))]
         Nothing (UnGuardedRhs (fromEP 0 cnt vci)) (BDecls [])]
 
 bdeclTo :: Int -> VCInfo -> Decl
-bdeclTo cnt vci@(VCInfo n ar i f as rs) =
+bdeclTo cnt vci =
     FunBind [Match srcLoc (Ident toFunName)
         [toEP 0 cnt vci]
         Nothing (UnGuardedRhs $ mkToRhs vci) (BDecls [])]
 
 mkToRhs :: VCInfo -> Exp
 mkToRhs (VCInfo n _ _ _ _ []) = Con (UnQual (Ident n))
-mkToRhs (VCInfo n _ _ _ _ rs) = buildProd rs
+mkToRhs (VCInfo n a _ _ _ rs) = buildProd a
 
-buildProd :: [Record] -> Exp       
-buildProd rs = buildInApp $ reverse (P.map recTyname rs)
+buildProd :: Int -> Exp
+buildProd rs = buildInApp $ reverse (genNames rs)
 
 buildInApp :: [String] -> Exp
 buildInApp [x]    = mkIdent x
@@ -76,14 +76,14 @@ pAppConUnQualIdent :: String -> Exp -> Exp
 pAppConUnQualIdent s e = Paren (App (conUnQualIdent s) e)
 
 fromEP :: Int -> Int -> VCInfo -> Exp
-fromEP _   1  vci = mkFromRs $ conRecords vci
-fromEP cnt nc vci@(VCInfo _ _ i _ _ rs)
-    | i == cnt + 1 && i == nc - 1 = pAppConUnQualIdent "R" $ mkFromRs rs
-    | i == cnt  = pAppConUnQualIdent "L" $ mkFromRs rs
+fromEP _   1  vci = mkFromRs $ M.conArity vci
+fromEP cnt nc vci@(VCInfo _ a i _ _ _)
+    | i == cnt + 1 && i == nc - 1 = pAppConUnQualIdent "R" $ mkFromRs a
+    | i == cnt  = pAppConUnQualIdent "L" $ mkFromRs a
     | otherwise = App (conUnQualIdent "R") (fromEP (cnt + 1) nc vci)
 
-mkFromRs :: [Record] -> Exp
-mkFromRs [] = mkIdent unitType
+mkFromRs :: Int -> Exp
+mkFromRs 0  = mkIdent unitType
 mkFromRs rs = buildProd rs
 
 ppPAppConUnQualIdent :: String -> Pat -> Pat
@@ -92,12 +92,12 @@ ppPAppConUnQualIdent s e = PParen (PApp (unQualIdent s) [e])
 mkPIdent :: String -> Pat
 mkPIdent = PVar . Ident 
 
-mkPRs :: [Record] -> Pat
-mkPRs [] = mkPIdent unitType
+mkPRs :: Int -> Pat
+mkPRs 0 = mkPIdent unitType
 mkPRs rs = buildPProd rs
 
-buildPProd :: [Record] -> Pat       
-buildPProd rs = buildInPApp $ reverse (P.map recTyname rs)
+buildPProd :: Int -> Pat
+buildPProd rs = buildInPApp $ reverse (genNames rs)
 
 buildInPApp :: [String] -> Pat
 buildInPApp [x]    = mkPIdent x
@@ -107,10 +107,15 @@ expPProd :: QName
 expPProd = (UnQual . Symbol) ":*:"
 
 toEP :: Int -> Int -> VCInfo -> Pat
-toEP _   1  vci = mkPRs $ conRecords vci
-toEP cnt nc vci@(VCInfo _ _ i _ _ rs)
-    | i == cnt + 1 && i == nc - 1 = ppPAppConUnQualIdent "R" $ mkPRs rs
-    | i == cnt  = ppPAppConUnQualIdent "L" $ mkPRs rs
+toEP _   1  vci = mkPRs $ M.conArity vci
+toEP cnt nc vci@(VCInfo _ a i _ _ _)
+    | i == cnt + 1 && i == nc - 1 = ppPAppConUnQualIdent "R" $ mkPRs a
+    | i == cnt  = ppPAppConUnQualIdent "L" $ mkPRs a
     | otherwise = PParen (PApp (unQualIdent "R") [(toEP (cnt + 1) nc vci)])
 
+genNames :: Int -> [String]
+genNames n = take n genNames'
+
+genNames' :: [String]
+genNames' = P.map (\x -> 'a' : P.show x) [1..]
 
