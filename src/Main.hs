6ea844debc90718bfe97e68b68cf231a42dbdf7e
Jurriën Stutterheim <j.stutterheim@me.com>
2010-12-25 19:58:47 +0100

    Update types for Main

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,20 @@
 module Main where
 
 import Data.Typeable
+import Language.Haskell.Exts
 import System.Console.CmdArgs
+import Data.Generics
 
-data DGGArgs = DGGArgs { adapter  :: String
+-- TODO: This probably shouldn't be a string, but some datatype representing
+-- the adapter. The adapter needs to be called in mkSrc.
+-- Each of the adapters needs to have its own "main" function with a unique
+-- name. A naming convention needs to be created for this. Initially, the 
+-- easiest approach would be to just hardcode all supported adapters. Later,
+-- dynamic adapter support could be investigated, possibly using variability
+-- mechanisms.
+type Adapter = String
+
+data DGGArgs = DGGArgs { adapter  :: Adapter
                        , finput   :: String
                        , datatype :: String
                        , foutput  :: String
@@ -23,7 +34,8 @@ dgg = DGGArgs { adapter  = def &= help "Adapter name. E.g.: EMGM"
 main :: IO ()
 main = do
     args <- cmdArgs dgg
-    code <- return $ genCode $ finput args
+    pr   <- parseFile (finput args) 
+    code <- return $ genCode pr (adapter args)
     if hasFileOutput args
         then writeFile (foutput args) code
         else putStrLn code
@@ -31,5 +43,21 @@ main = do
 hasFileOutput :: DGGArgs -> Bool
 hasFileOutput dgg = not $ null $ foutput dgg
 
-genCode :: String -> String
-genCode = id
+genCode :: ParseResult Module -> Adapter -> String
+genCode (ParseFailed l m) _ = error $ "Failed to parse module. Error on line " ++ show l ++ ": " ++ m 
+genCode (ParseOk m)       a = prettyPrint (mkSrc a $ listify isSuppDecl m)
+
+mkSrc :: Adapter -> [Decl] -> Decl
+mkSrc a []     = undefined
+mkSrc a (x:xs) = undefined
+
+-- Returns True when the Decl is of the right type and False otherwise. Several
+-- types return False at the moment, because they are not supported yet by this
+-- library. Support for these types is planned for future increments.
+isSuppDecl :: Decl -> Bool
+isSuppDecl (TypeDecl _ _ _ _)           = False
+isSuppDecl (TypeFamDecl _ _ _ _)        = False
+isSuppDecl (DataDecl _ _ _ _ _ _ _)     = True
+isSuppDecl (GDataDecl _ _ _ _ _ _ _ _)  = False
+isSuppDecl (DataFamDecl _ _ _ _ _)      = False
+isSuppDecl _                            = False
