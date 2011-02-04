c2235721a9b8fefbbc1cb153f649a1fd2bd4baf4
Jurriën Stutterheim <j.stutterheim@me.com>
2011-02-04 09:52:14 +0100

    Add HSEParser utility

---
+++ src/HSEParser.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import System (getArgs)
+import Language.Haskell.Exts
+
+main :: IO ()
+main =
+    do args <- getArgs
+       if length args == 0
+           then error "Filename required"
+           else return ()
+       pr <- parseFile $ head args
+       do case pr of
+            (ParseOk a)       -> processModule a 
+            (ParseFailed _ m) -> putStrLn m
+
+processModule :: Module -> IO ()
+processModule (Module _ _ _ _ _ _ ds) = sequence_ $ map (\x -> putStrLn (show x ++ "\n")) ds 
+
