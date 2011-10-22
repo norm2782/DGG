e35d8038b9d55dde27f0a2453158101d5a2e4f48
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-22 15:27:18 +0200

    Expand types a bit

--- src/DGG/Types.hs
+++ src/DGG/Types.hs
@@ -1,20 +1,30 @@
 module DGG.Types where
 
+import GHC
 import HsTypes
+
+data DataType = DataType {
+     tyCon  :: TyCon
+  ,  ctors  :: [Ctor]
+}
 
 type Name = String
 
-data Ctor = Ctor CName Fixity Fields
+data TyCon = TyCon {
+     tyConName  :: Name
+  ,  tyKind     :: Kind
+  ,  tyVars     :: [Name]
+}
+
+data Ctor = Ctor {
+     ctorName    :: CName
+  ,  ctorFixity  :: Fixity
+  ,  ctorFields  :: Fields
+}
 
 data Fields = Fields [HsType]
             | RecordFields [(CName, HsType)]
 
 data CName = Ident String (Maybe String)
            | Symbol String String
-
-data DataType = DataType {
-     tyConName  :: Name
-  ,  tyVars     :: [Name]
-  ,  ctors      :: [Ctor]
-}
 
