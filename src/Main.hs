a2207240eb53498f6516bb619b5c5222518bf71d
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-22 09:07:04 +0200

    Throw error on missing CLI arguments

--- src/Main.hs
+++ src/Main.hs
@@ -13,31 +13,33 @@ import DGG.Adapter.MultiRec
 import DGG.Adapter.SYB
 import System.Console.CmdArgs
 
-data DGGArgs = DGGArgs { adapter  :: String
-                       , input    :: String
---                       , datatype :: String
-                       , modulename  :: String
-                       , output   :: String
-                       }
-                       deriving (Show, Data, Typeable)
+data DGGArgs = DGGArgs {
+     adapter     :: String
+  ,  input       :: String
+--  ,  datatype    :: String
+  ,  modulename  :: String
+  ,  output      :: String
+  }
+  deriving (Show, Data, Typeable)
 
 -- TODO: Add output module name as parameter
 dgg :: DGGArgs
-dgg = DGGArgs { adapter    = def &= help "Adapter name. E.g.: EMGM"
-              , input      = def &= typFile &= help "Input file"
---              , datatype = def &= help "Specify datatype for which to derive. Generates for all datatypes if left blank."
-              , modulename = def &= help "Name of the module. Defaults to GenericReps."
-              , output     = def &= typFile &= help "Output file. E.g.: Instances.hs"
-              }
-              &= summary "DGG: Datatype Generic Generator v0.1.1-dev"
-              &= program "dgg"
+dgg = DGGArgs {
+     adapter    = def &= help "Adapter name. E.g.: EMGM"
+  ,  input      = def &= typFile &= help "Input file"
+--   ,  datatype = def &= help "Specify datatype for which to derive. Generates for all datatypes if left blank."
+  ,  modulename = def &= help "Name of the module. Defaults to GenericReps."
+  ,  output     = def &= typFile &= help "Output file. E.g.: Instances.hs"
+  }
+  &= summary "DGG: Datatype Generic Generator v0.2-dev"
+  &= program "dgg"
 
 main :: IO ()
 main = do
     args <- cmdArgs dgg
     -- TODO: This is rather ugly. Can't CmdArgs do this kind of thing?
-    when ((null . adapter) args || (null . input) args)
-         undefined -- error "Specify at least an adapter and an input file."
+    when ((null . adapter) args || (null . input) args) $
+         error "Specify at least an adapter and an input file."
     let mn | null $ modulename args  = "GenericReps"
            | otherwise               = modulename args
     pr   <- parseFile (input args)
@@ -59,6 +61,7 @@ mkModule _ _ _  [] = error "No compatible datatypes found."
 mkModule n p is xs = Module srcLoc (ModuleName n) [] Nothing Nothing is
                             $ concatMap (p . mkTCI) xs
 
+-- TODO: I don't like the way this is done here...
 data Adapters = EMGM | SYB | MultiRec
 
 class Adapter a where
