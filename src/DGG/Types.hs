25417a5208e6505f0a0f2caef87f05ee5d20378a
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-23 12:45:19 +0200

    Cleaning up: removing uu-parsinglib for now

--- src/DGG/Types.hs
+++ src/DGG/Types.hs
@@ -1,20 +1,7 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE Rank2Types #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE EmptyDataDecls #-}
-{-# LANGUAGE KindSignatures #-}
-
-
 module DGG.Types where
-
--- General imports
-import Text.ParserCombinators.UU
-import Text.ParserCombinators.UU.BasicInstances
-import Text.ParserCombinators.UU.Utils
 
 -- GHC related imports
 import GHC hiding (Kind, Fixity)
-import HsTypes
 
 data DataType name = DataType {
      tyCon  :: TypeCon
@@ -58,38 +45,4 @@ data Fields name = Fields [HsType name]
 data CName = Ident String (Maybe String)
            | Symbol String String
 
-{- data EMGM-}
-{- data MultiRec-}
 
-{- data Lib :: * -> * where-}
-  {- EMGMLib :: Lib EMGM-}
-  {- MultiRecLib :: Lib MultiRec-}
-
-{- class Adapter a where-}
-  {- gencode     :: Lib a -> [DataType name] -> m [HsDecl name]-}
-  {- imports     :: Lib a -> [String]-}
-  {- extensions  :: Lib a -> [String]-}
-
-{- instance Adapter EMGM where-}
-  {- gencode     _ = undefined-}
-  {- imports     _ = []-}
-  {- extensions  _ = []-}
-
-{- instance Adapter MultiRec where-}
-  {- gencode     _ = undefined-}
-  {- imports     _ = []-}
-  {- extensions  _ = []-}
-
-{- parseAdapter :: Parser (Lib a)-}
-{- parseAdapter = pEMGM <|> pMultiRec-}
-
-{- pEMGM :: Parser (Lib a)-}
-{- pEMGM = EMGMLib <$ pSymbol "emgm"-}
-
-{- pMultiRec :: Parser (Lib a)-}
-{- pMultiRec = MultiRecLib <$ pSymbol "multirec"-}
-
-
-{- startParse :: Parser a -> String ->  (a, [Error LineColPos])-}
-{- startParse p inp  =  parse ((,) <$> p <*> pEnd)-}
-                  {- $  createStr (LineColPos 0 0 0) inp-}
