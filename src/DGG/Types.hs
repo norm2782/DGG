12c449124d9c5302d9d2b049a754beadb2f12453
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-23 12:37:14 +0200

    Add kind and fixity

--- src/DGG/Types.hs
+++ src/DGG/Types.hs
@@ -1,30 +1,95 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE KindSignatures #-}
+
+
 module DGG.Types where
 
-import GHC
+-- General imports
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU.BasicInstances
+import Text.ParserCombinators.UU.Utils
+
+-- GHC related imports
+import GHC hiding (Kind, Fixity)
 import HsTypes
 
-data DataType = DataType {
-     tyCon  :: TyCon
-  ,  ctors  :: [Ctor]
+data DataType name = DataType {
+     tyCon  :: TypeCon
+  ,  ctors  :: [Ctor name]
 }
 
-type Name = String
+type TName = String
 
-data TyCon = TyCon {
-     tyConName  :: Name
+-- Stolen from HSE
+data Kind
+    = KindStar
+    | KindBang
+    | KindFn Kind Kind
+    | KindParen Kind
+    | KindVar TName
+
+data TypeCon = TypeCon {
+     tyConName  :: TName
   ,  tyKind     :: Kind
-  ,  tyVars     :: [Name]
+  ,  tyVars     :: [TName]
 }
 
-data Ctor = Ctor {
+-- Stolen from HSE
+data Assoc
+     = AssocNone
+     | AssocLeft
+     | AssocRight
+
+-- Stolen from HSE
+data Fixity = Fixity Assoc Int
+
+data Ctor name = Ctor {
      ctorName    :: CName
   ,  ctorFixity  :: Fixity
-  ,  ctorFields  :: Fields
+  ,  ctorFields  :: Fields name
 }
 
-data Fields = Fields [HsType]
-            | RecordFields [(CName, HsType)]
+data Fields name = Fields [HsType name]
+                 | RecordFields [(CName, HsType name)]
 
 data CName = Ident String (Maybe String)
            | Symbol String String
 
+{- data EMGM-}
+{- data MultiRec-}
+
+{- data Lib :: * -> * where-}
+  {- EMGMLib :: Lib EMGM-}
+  {- MultiRecLib :: Lib MultiRec-}
+
+{- class Adapter a where-}
+  {- gencode     :: Lib a -> [DataType name] -> m [HsDecl name]-}
+  {- imports     :: Lib a -> [String]-}
+  {- extensions  :: Lib a -> [String]-}
+
+{- instance Adapter EMGM where-}
+  {- gencode     _ = undefined-}
+  {- imports     _ = []-}
+  {- extensions  _ = []-}
+
+{- instance Adapter MultiRec where-}
+  {- gencode     _ = undefined-}
+  {- imports     _ = []-}
+  {- extensions  _ = []-}
+
+{- parseAdapter :: Parser (Lib a)-}
+{- parseAdapter = pEMGM <|> pMultiRec-}
+
+{- pEMGM :: Parser (Lib a)-}
+{- pEMGM = EMGMLib <$ pSymbol "emgm"-}
+
+{- pMultiRec :: Parser (Lib a)-}
+{- pMultiRec = MultiRecLib <$ pSymbol "multirec"-}
+
+
+{- startParse :: Parser a -> String ->  (a, [Error LineColPos])-}
+{- startParse p inp  =  parse ((,) <$> p <*> pEnd)-}
+                  {- $  createStr (LineColPos 0 0 0) inp-}
--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,23 @@
 
 module Main where
 
+-- DGG imports
+import DGG.Types
+
+-- General imports
 import Control.Monad
 import Data.Char
 import Data.Generics
 import Data.Map (fromList, (!), Map)
 import Data.Typeable
 import System.Console.CmdArgs
+
+-- GHC related imports
+import DynFlags
+import GHC
+import GHC.Paths ( libdir )
+import HsDecls
+import Outputable
 
 data DGGArgs = DGGArgs {
      adapter     :: String
@@ -29,18 +40,47 @@ dgg = DGGArgs {
   &= summary "DGG: Datatype Generic Generator v0.2-dev"
   &= program "dgg"
 
-main = undefined
-{- main :: IO ()-}
-{- main = do-}
-  {- args <- cmdArgs dgg-}
+
+
+
+main :: IO ()
+main = do
+  args <- cmdArgs dgg
   {- TODO: This is rather ugly. Can't CmdArgs do this kind of thing?-}
-  {- when ((null . adapter) args || (null . inputfile) args) $-}
-      {- error "Specify at least an adapter and an input file."-}
-  {- let mn | null $ modulename args  = "GenericReps"-}
-         {- | otherwise               = modulename args-}
-  {- pr   <- parseFile (input args)-}
-  {- let adap  = adapters ! map toLower (adapter args)-}
-  {- let code  = genCode pr mn (makeFn adap) (isSuppFn adap) (importsFn adap)-}
-  {- if (not . null . output) args-}
-    {- then writeFile (output args) code-}
-    {- else putStrLn code-}
+  when ((null . adapter) args || (null . inputfile) args) $
+      error "Specify at least an adapter and an input file."
+  let mn | null $ modulename args  = "GenericReps"
+         | otherwise               = modulename args
+  pr <- parseFile (inputfile args)
+  let adap  = undefined -- adapters ! map toLower (adapter args)
+  let code  = undefined -- genCode pr mn (makeFn adap) (isSuppFn adap) (importsFn adap)
+  if (not . null . outputfile) args
+    then writeFile (outputfile args) code
+    else putStrLn code
+
+
+parseFile :: String -> IO ModSummary
+parseFile filename =
+  defaultErrorHandler defaultLogAction $ do
+    runGhc (Just libdir) $ do
+      dflags <- getSessionDynFlags
+      let dflags' = foldl xopt_set dflags
+                          [Opt_Cpp, Opt_ImplicitPrelude, Opt_MagicHash]
+      setSessionDynFlags dflags'
+      target <- guessTarget filename Nothing
+      setTargets [target]
+      load LoadAllTargets
+      mods <- getModuleGraph
+      let modSum =  case mods of
+                      []     -> error "No modules loaded"
+                      (x:_)  -> x
+      p <- parseModule modSum
+      t <- typecheckModule p
+      d <- desugarModule t -- TODO: We don't need this
+      l <- loadModule d
+      n <- getNamesInScope
+      c <- return $ coreModule d
+
+      g <- getModuleGraph
+      mapM showModule g
+      return $ pm_mod_summary p -- $ (parsedSource d)--,"\n-----\n",  typecheckedSource d)
